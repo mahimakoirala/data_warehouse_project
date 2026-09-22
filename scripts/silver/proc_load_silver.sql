192a333b564cf3adc0185087a5d213fbe15d8f25
mahimakoirala <134276115+mahimakoirala@users.noreply.github.com>
2026-09-22 17:06:46 +0545

    Create proc_load_silver.sql

---
+++ scripts/silver/proc_load_silver.sql
@@ -0,0 +1,120 @@
+/*
+=======================================================
+Stored Procedure: Load Silver Layer ( Bronze -> Silver)
+=======================================================
+Script Purpose:
+    This SP performs the ETL (Extract, Transform, Load) process to populate 
+    the 'silver' schema tables from the 'bronze' schema.
+Actions performed:
+    - Truncates Silver Tables.
+    - Inserts transformed and cleaned data from bronze into Silver tables.
+
+Parameters:
+    None.
+    This SP does not accept any parameters or return values.
+
+Usage Example:
+    EXEC silver.load_silver;
+==========================================================
+*/
+
+CREATE or ALTER PROCEDURE silver.load_silver AS
+BEGIN
+    DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+    BEGIN TRY
+        SET @batch_start_time = GETDATE();
+        PRINT '================================';
+		PRINT 'Loading the Silver Layer';
+		PRINT '================================';
+
+		PRINT '--------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '--------------------';
+
+        --Loading silver.crm_cust_info;
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.crm_cust_info';
+		TRUNCATE TABLE silver.crm_cust_info;
+		PRINT '>> Inserting Data into: silver.crm_cust_info';
+        INSERT INTO silver.crm_cust_info (
+            cst_id,
+            cst_key,
+            cst_firstname,
+            cst_lastname,
+            cst_marital_status,
+            cst_gndr,
+            cst_create_date)
+
+        SELECT 
+        cst_id,
+        cst_key,
+        TRIM(cst_firstname) AS cst_firstname,
+        TRIM(cst_lastname) as cst_lastname,
+        CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+            WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+            ELSE 'n/a'
+        END cst_marital_status,
+        CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+            WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+            ELSE 'n/a'
+        END cst_gndr,
+        cst_create_date
+        FROM (
+        SELECT 
+            *, ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+            FROM bronze.crm_cust_info 
+            WHERE cst_id is not null
+        )t WHERE flag_last = 1; --select the most recent record per customer
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'seconds';
+        PRINT '>> ------------------------------';
+
+        --Loading silver.crm_prd_info
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.crm_prd_info';
+		TRUNCATE TABLE silver.crm_prd_info;
+		PRINT '>> Inserting Data Into: silver.crm_prd_info';
+        INSERT INTO silver.crm_prd_info (
+	        prd_id,
+	        cat_id,
+	        prd_key,
+	        prd_nm,
+	        prd_cost,
+	        prd_line,
+	        prd_start_dt,
+	        prd_end_dt
+        )
+        SELECT 
+        prd_id, 
+        REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') as cat_id, -- Extract Category ID
+        SUBSTRING(prd_key,7, len(prd_key)) AS prd_key,		  -- Extract Product Key
+        prd_nm,
+        COALESCE(prd_cost, 0) as prd_cost,
+        CASE UPPER(TRIM(prd_line))
+	        WHEN 'M' THEN 'Mountain'
+	        WHEN 'R' THEN 'Road'
+	        WHEN 'S' THEN 'Sales'
+	        WHEN 'T' THEN 'Touring'
+	        ELSE 'n/a'
+        END as prd_line, --Map product line codes to descriptive values
+        CAST(prd_start_dt AS DATE),
+        CAST(
+	        LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) -1 
+	        AS DATE)
+	        AS prd_end_dt   --Calculate end date as one day before the next start date
+        FROM bronze.crm_prd_info;
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> -------------';
+
+    END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
+END
+

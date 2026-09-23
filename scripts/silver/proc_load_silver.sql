f5a7fe52dc27e26e27a56d5a8c0fd4d9d882f537
mahimakoirala <134276115+mahimakoirala@users.noreply.github.com>
2026-09-23 15:45:00 +0545

    Add data loading for silver.crm_sales_details

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -107,6 +107,51 @@ BEGIN
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
         PRINT '>> -------------';
 
+		 --Loading silver.crm_sales_details
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.crm_sales_details';
+		TRUNCATE TABLE silver.crm_sales_details;
+		PRINT '>> Inserting Data Into: silver.crm_sales_details';
+		INSERT INTO silver.crm_sales_details (
+	       [sls_ord_num]
+	      ,[sls_prd_key]
+	      ,[sls_cust_id]
+	      ,[sls_order_dt]
+	      ,[sls_ship_dt]
+	      ,[sls_due_dt]
+	      ,[sls_sales]
+	      ,[sls_quantity]
+	      ,[sls_price]
+		)
+		
+		SELECT 
+		       [sls_ord_num],
+		      [sls_prd_key],
+		      [sls_cust_id],
+		      CASE WHEN sls_order_dt = 0 OR len(sls_order_dt) !=8 THEN NULL
+		            ELSE CAST(CAST(sls_order_dt as VARCHAR) AS DATE)
+		       END AS sls_order_dt,
+		       CASE WHEN [sls_ship_dt] = 0 OR len([sls_ship_dt]) !=8 THEN NULL
+		            ELSE CAST(CAST([sls_ship_dt] as VARCHAR) AS DATE)
+		       END AS [sls_ship_dt],
+		       CASE WHEN [sls_due_dt] = 0 OR len([sls_due_dt]) !=8 THEN NULL
+		            ELSE CAST(CAST([sls_due_dt] as VARCHAR) AS DATE)
+		       END AS [sls_due_dt],
+		      CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+				THEN sls_quantity * ABS(sls_price)
+			    ELSE sls_sales
+		    END AS sls_sales,
+		    [sls_quantity],
+		    CASE WHEN sls_price IS NULL OR sls_price <=0 
+				    THEN sls_sales / NULLIF(sls_quantity, 0)
+			    ELSE sls_price
+		    END AS sls_price
+		  	FROM [DataWarehouse].[bronze].[crm_sales_details];
+			SET @end_time = GETDATE();
+	        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+	        PRINT '>> -------------';			
+
+
     END TRY
 	BEGIN CATCH
 		PRINT '=========================================='

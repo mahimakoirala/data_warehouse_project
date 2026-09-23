adcc2d9fc0015417fe20c0f6723b329cc339a0fd
mahimakoirala <134276115+mahimakoirala@users.noreply.github.com>
2026-09-23 16:19:41 +0545

    Create erp_cust_az12 table in silver schema

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -59,3 +59,16 @@ CREATE TABLE silver.crm_sales_details (
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
+
+IF OBJECT_ID ('[silver].[erp_cust_az12]', 'U') IS NOT NULL
+	DROP TABLE [silver].[erp_cust_az12];
+GO
+
+CREATE TABLE [silver].[erp_cust_az12](
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen varchar(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
